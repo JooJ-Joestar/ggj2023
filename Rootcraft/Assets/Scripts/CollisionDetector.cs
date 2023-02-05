1ff2c31799538247db7a465db98c70e590e5db0e
Lincoln Batista <linkskill@gmail.com>
2023-02-05 05:45:21 -0300

    initial screen improved and grab logic initialized (still pending)

--- Rootcraft/Assets/Scripts/CollisionDetector.cs
+++ Rootcraft/Assets/Scripts/CollisionDetector.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollisionDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    bool IsGrabbed;
+    string GrabbedRootName;
+    GameObject GrabbedRoot;
+    public Transform GrabPoint;
+    public float GrabbedObjectYValue;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GrabAndDrop();
+        }
     }
+
+    public void GrabAndDrop()
+    {
+        if (IsGrabbed)
+        {
+            IsGrabbed = false;
+            GrabbedRootName = "";
+        }
+        else
+        {
+            IsGrabbed = true;
+            GrabbedRoot.transform.parent = transform;
+            GrabbedObjectYValue = GrabbedRoot.transform.position.y;
+            GrabbedRoot.transform.localPosition = GrabPoint.localPosition;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Root" && IsGrabbed == false)
+        {
+            GrabbedRoot = collision.gameObject;
+            GrabbedRootName = GrabbedRoot.name;
+        }
+        if (collision.gameObject.tag == "Cauldron")
+        {
+            if (IsGrabbed)
+            {
+                Debug.Log(GrabbedRootName);
+                IsGrabbed = false;
+            }
+        }
+    }
+
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Root")
+    //    {
+    //        IsRootStayed = true;
+    //    }
+    //}
 }
